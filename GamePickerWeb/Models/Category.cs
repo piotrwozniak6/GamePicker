2a32fbbc76d104918a9152936b08c844f4bca904
Piotr Woźniak <piotr.wozniak.job@gmail.com>
2024-02-28 19:20:56 +0100

    server side validations

--- GamePickerWeb/Models/Category.cs
+++ GamePickerWeb/Models/Category.cs
@@ -4,8 +4,11 @@ namespace GamePickerWeb.Models;
 
 public class Category
 {
+    [Key]
     public int Id { get; set; }
     [Required]
+    [MaxLength(25)]
     public string Name { get; set; }
+    [Range(1, 50, ErrorMessage = "Order must be between 1-50")]
     public int Order { get; set; }
 }
