d423caf85ec44ee2ea4e1221faf0351684f79940
Piotr Woźniak <piotr.wozniak.job@gmail.com>
2024-03-16 07:53:07 +0100

    reactor

---
+++ GamePickerDataAccess/Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+using GamePickerModels.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GamePickerDataAccess.Data;
+
+public class ApplicationDbContext: DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
+    {
+        
+    }
+
+    public DbSet<Category> Categories { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Category>().HasData(
+            new Category { Id = 1, Name = "Action", Order = 1},
+            new Category { Id = 2, Name = "Shooter", Order = 2},
+            new Category { Id = 3, Name = "Sports", Order = 3}
+            );
+    }
+}
