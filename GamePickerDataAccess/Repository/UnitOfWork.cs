fafad6ff9a782bab84e3290b8455fe879d03c864
Piotr Woźniak <piotr.wozniak.job@gmail.com>
2024-03-18 19:14:10 +0100

    implementing unit of work

---
+++ GamePickerDataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,21 @@
+using GamePickerDataAccess.Data;
+using GamePickerDataAccess.Repository.IRepository;
+
+namespace GamePickerDataAccess.Repository;
+
+public class UnitOfWork: IUnitOfWork
+{
+    private ApplicationDbContext _db;
+    public ICategoryRepository CategoryRepository { get; private set; }
+    
+    public UnitOfWork(ApplicationDbContext db) 
+    {
+        _db = db;
+        CategoryRepository = new CategoryRepository(_db);
+    }
+    
+    public void Save()
+    {
+        
+    }
+}
