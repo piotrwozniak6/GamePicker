d7159819056ad0684735451a20c86cfadea134f5
Piotr Woźniak <piotr.wozniak.career@gmail.com>
2024-03-31 19:17:16 +0200

    - refactoring updating pictures

--- GamePickerDataAccess/Repository/GameModelRepository.cs
+++ GamePickerDataAccess/Repository/GameModelRepository.cs
@@ -15,6 +15,22 @@ public class GameModelRepository: Repository<GameModel>, IGameModelRepository
     
     public void Update(GameModel item)
     {
-        _db.Update(item);
+        // _db.Update(item);
+        var itemDb = _db.GameModels.FirstOrDefault(u=>u.Id==item.Id);
+        if (item != null)
+        {
+            itemDb.Title = item.Title;
+            itemDb.Description= item.Description;
+            itemDb.Publisher= item.Publisher;
+            itemDb.RegularPrice= item.RegularPrice;
+            itemDb.Price= item.Price;
+            itemDb.Price50= item.Price50;
+            itemDb.Price100= item.Price100;
+            itemDb.Id = item.Id;
+            if (item.ImageUrl != null)
+            {
+                itemDb.ImageUrl = item.ImageUrl;
+            }
+        }
     }
 }
--- GamePickerWeb/Areas/Admin/Controllers/GameModelController.cs
+++ GamePickerWeb/Areas/Admin/Controllers/GameModelController.cs
@@ -12,13 +12,15 @@ namespace GamePickerWeb.Areas.Admin.Controllers;
 public class GameModelController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
-    public GameModelController(IUnitOfWork unitOfWork)
+    private readonly IWebHostEnvironment _webHostEnvironment;
+    public GameModelController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
     {
         _unitOfWork= unitOfWork;
+        _webHostEnvironment = webHostEnvironment;
     }
     public IActionResult Index()
     {
-        List<GameModel> objGameModelList = _unitOfWork.GameModelRepository.GetAll().ToList();
+        List<GameModel> objGameModelList = _unitOfWork.GameModelRepository.GetAll(includeItems:"Category").ToList();
         return View(objGameModelList);
     }
 
@@ -51,7 +53,38 @@ public class GameModelController : Controller
     {
         if (ModelState.IsValid)
         {
-            _unitOfWork.GameModelRepository.Add(gameModelVm.GameModel);
+            string wwwRootPath = _webHostEnvironment.WebRootPath;
+            if (file != null)
+            {
+                string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                string productPath = Path.Combine(wwwRootPath, @"images/gameModel");
+
+                if (!string.IsNullOrEmpty(gameModelVm.GameModel.ImageUrl))
+                {
+                    var oldImagePath = Path.Combine(wwwRootPath, gameModelVm.GameModel.ImageUrl.TrimStart('/'));
+
+                    if (System.IO.File.Exists(oldImagePath))
+                    {
+                        System.IO.File.Delete(oldImagePath);
+                    }
+                }
+                
+                using (var fileStream = new FileStream(Path.Combine(productPath, fileName), FileMode.Create))
+                {
+                    file.CopyTo(fileStream);
+                }
+
+                gameModelVm.GameModel.ImageUrl = @"/images/gameModel/" + fileName;
+            }
+
+            if (gameModelVm.GameModel.Id == 0)
+            {
+                _unitOfWork.GameModelRepository.Add(gameModelVm.GameModel);
+            }
+            else
+            {
+                _unitOfWork.GameModelRepository.Update(gameModelVm.GameModel);
+            }
             _unitOfWork.Save();
             TempData["success"] = "GameModel created successfully";
             
