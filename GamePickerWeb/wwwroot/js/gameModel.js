38e470625ff7f28f26e30c9a49a14e2370310405
Piotr Woźniak <piotr.wozniak.career@gmail.com>
2024-04-19 16:17:29 +0200

    refactoring delete button and implementing sweetalert2 for it

--- GamePickerWeb/wwwroot/js/gameModel.js
+++ GamePickerWeb/wwwroot/js/gameModel.js
@@ -17,7 +17,7 @@ function loadDataTable() {
                     <a href="/admin/gamemodel/upsert?id=${data}" class="btn btn-outline-primary btn-sm">
                         <i class="bi bi-pencil"></i> Edit
                     </a>
-                    <a class="btn btn-outline-danger btn-sm">
+                    <a onClick=Delete('/admin/gamemodel/delete/${data}') class="btn btn-outline-danger btn-sm">
                         <i class="bi bi-trash3"></i> Delete
                     </a>
                     </div>`
@@ -27,3 +27,26 @@ function loadDataTable() {
         ]
     });
 }           
+
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    dataTable.ajax.reload();
+                    toastr.success(data.message);
+                } 
+            })
+        }
+    });
+}
