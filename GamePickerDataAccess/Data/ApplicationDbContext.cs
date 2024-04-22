09fa28cf08ed7e58f11261fe39fee2524cb98d57
Piotr Woźniak <piotr.wozniak.career@gmail.com>
2024-04-22 19:15:03 +0200

    implementing scaffolded identity

--- GamePickerDataAccess/Data/ApplicationDbContext.cs
+++ GamePickerDataAccess/Data/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
 using GamePickerModels.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace GamePickerDataAccess.Data;
 
-public class ApplicationDbContext: DbContext
+public class ApplicationDbContext: IdentityDbContext<IdentityUser>
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base(options)
     {
@@ -12,9 +14,12 @@ public class ApplicationDbContext: DbContext
 
     public DbSet<Category> Categories { get; set; }
     public DbSet<GameModel> GameModels { get; set; }
+    public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        base.OnModelCreating(modelBuilder);
+        
         modelBuilder.Entity<Category>().HasData(
             new Category { Id = 1, Name = "Action", Order = 1},
             new Category { Id = 2, Name = "Shooter", Order = 2},
