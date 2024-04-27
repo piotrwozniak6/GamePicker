ab46cf7ad8064cd813aca335e1760bbc9d03e2ac
Piotr Woźniak <piotr.wozniak.career@gmail.com>
2024-04-27 13:52:27 +0200

    add 4 roles for different type of access and restrict access by providing Authorize controllers

--- GamePickerUtility/SD.cs
+++ GamePickerUtility/SD.cs
@@ -1,6 +1,9 @@
 namespace GamePickerUtility;
 
-static public class SD
+public static class SD
 {
-    
+    public const string Role_User_Customer = "Customer";
+    public const string Role_User_Company = "Company";
+    public const string Role_Admin = "Admin";
+    public const string Role_Employee= "Employee";
 }
